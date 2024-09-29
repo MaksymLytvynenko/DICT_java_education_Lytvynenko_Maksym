acac42a59695a35a5f1a4ff04e9f032924557c1f
Maksym <m.i.lytvynenko@student.khai.edu>
2024-09-29 14:00:34 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,16 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is ChatBot.");
         System.out.println("I was created in 2024.");
+        System.out.println("Please remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String Name = scanner.nextLine();
+
+        System.out.println("What a great name you have " + Name + "!");
     }
 }
